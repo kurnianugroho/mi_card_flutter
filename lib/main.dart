028ebd8889055c9e3cac9fe2a55af6f70bda28a9
kurnianugroho <kurnia.nugroho@gmail.com>
2022-12-04 14:18:13 +0700

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,98 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/kur.jpg'),
+              ),
+              Text(
+                'Kurnia Adi Nugroho',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                  letterSpacing: 2.0,
+                ),
+              ),
+              Text(
+                'MOBILE APP DEVELOPER',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade900,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    fontFamily: 'Source Sans Pro'),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade900,
+                  thickness: 0.8,
+                ),
+              ),
+              Card(
+                elevation: 10.0,
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 32.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      '+62 857 2704 5444',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 18.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                elevation: 10.0,
+                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 32.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      'kurnia.nugroho@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 18.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
